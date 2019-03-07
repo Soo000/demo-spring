336b79fe2eb363164394291227c779f798457c8b
wangke <49186456@qq.com>
2019-03-07 20:06:39 +0800

    增加事务demo

---
+++ demo-spring-4/src/main/java/com/kkwrite/demo/spring4/tx/dao/UserDao.java
@@ -0,0 +1,33 @@
+package com.kkwrite.demo.spring4.tx.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kkwrite.demo.spring4.tx.entity.User;
+
+@Repository
+public class UserDao {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public int save(User user) {
+		int result = 0;
+		
+		String sql = "insert into ics_user(username, company_id, password, visitor_num, e_mail, is_deleted, gmt_create, gmt_modified) "
+				+ "values (?, ?, ?, ?, ?, ?, ?, ?)";
+		result = jdbcTemplate.update(sql, 
+				user.getUsername(), 
+				user.getCompanyId(),
+				user.getPassword(),
+				user.getVisitorNum(),
+				user.getEmail(),
+				user.getDeleted(),
+				user.getGmtCreate(),
+				user.getGmtModified());
+		
+		return result;
+	}
+	
+}
